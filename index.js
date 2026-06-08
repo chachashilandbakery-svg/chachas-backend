88c787791c0f2bb5527a6bb43ed7ad59a4450596
chachashilandbakery-svg <chachashilandbakery@gmail.com>
2026-06-08 15:14:20 -0500

    Update index.js

--- index.js
+++ index.js
@@ -65,4 +65,4 @@ app.post('/order', async (req, res) => {
 app.get('/', (req, res) => res.json({ status: 'Chachas Bakery API running!' }));
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
